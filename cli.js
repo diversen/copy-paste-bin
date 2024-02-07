4ec210e5486afb772ef632258a66788f60dc898d
Dennis Iversen <dennis.iversen@gmail.com>
2024-02-07 19:23:44 +0100

    update deps. Refactor a bit

--- cli.js
+++ cli.js
@@ -3,11 +3,21 @@
 import minimist from 'minimist-mini';
 import * as ncp from "copy-paste";
 
+
 var opts = [];
 opts.boolean = ['paste', 'help'];
 opts.string = ['copy'];
 
-var m = minimist(opts);
+var minimistMini = minimist(opts);
+
+function ncpError(err) {
+    if (err) {
+        console.log('Error copying to clipboard')
+        console.log(err);
+        process.exit(1);
+    }
+}
+
 
 // Function to handle piped input
 function handlePipedInput() {
@@ -22,34 +32,40 @@ function handlePipedInput() {
     process.stdin.on('end', function () {
         if (data) {
             // If data is piped in, use it as if it was provided with --copy
-            ncp.copy(data, function () {
+            ncp.copy(data, function (err) {
+                ncpError(err);
                 process.exit(0);
             });
         } else {
             // No data piped in, and no other options provided
-            m.helpMessage();
+            minimistMini.helpMessage();
         }
     });
 }
 
 // Help
-if (m.get('help')) {
-    m.helpMessage();
+if (minimistMini.getOption('help')) {
+    minimistMini.helpMessage();
 }
 
 // Copy to clipboard
-if (m.get('copy')) {
-    ncp.copy(m.get('copy'), function () {
+if (minimistMini.getOption('copy')) {
+    ncp.copy(minimistMini.getOption('copy'), function (err) {
+        ncpError(err);
         process.exit(0);
     });
-} else if (m.get('paste')) {
-    console.log(ncp.paste());
-    process.exit(0);
+} else if (minimistMini.getOption('paste')) {
+    ncp.paste(function (err, data) {
+        ncpError(err);
+        console.log(data);  
+    });
+    
 } else {
     // Check if there is piped input
     if (!process.stdin.isTTY) {
+        console.log('Piped input detected');
         handlePipedInput();
-    } else if (!m.get('copy') && !m.get('paste')) {
-        m.helpMessage();
+    } else if (!minimistMini.getOption('copy') && !minimistMini.getOption('paste')) {
+        minimistMini.helpMessage();
     }
 }
